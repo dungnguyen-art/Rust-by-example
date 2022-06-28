2b6c0dbdb13b20400b8ec0db41f1884a79f3c335
dungnguyen-art <dungtpa2@gmail.com>
2022-06-28 09:21:21 +0700

    handle input from keyboard

--- Rust_Programming/src/string_slide.rs
+++ Rust_Programming/src/string_slide.rs
@@ -1,12 +1,18 @@
+use std::io;
+
 fn main(){
    let split = "This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal.".split(" ");
-   let str_cnt = "is";
+   let mut str_cnt = String::new();
+   io::stdin().read_line(&mut str_cnt).expect("could read file");
+   let new_str_cnt = str_cnt.to_string();
+   let new_str_cnt = new_str_cnt.trim();
+
    let mut _cnt:i32 = 0;
     for s in split {
-        let s = &s.replace(".","");
-        if s == str_cnt {
-            _cnt += 1;
-        }
+        let new_string = String::from(&s.replace(".",""));
+        if new_string == new_str_cnt {
+           _cnt += 1;
+       }
     }
     println!("{}",_cnt);
 }
